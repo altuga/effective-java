41bf8d560fe58f0c98ac0d9c4241e883f25b026f
Altug B. Altintas <altuga@gmail.com>
2022-12-05 14:54:09 +0300

    Basic constructor örneği

--- src/main/java/org/jugistanbul/question01/NutritionFacts.java
+++ src/main/java/org/jugistanbul/question01/NutritionFacts.java
@@ -2,10 +2,8 @@ package org.jugistanbul.question01;
 
 /**
  * Altug Bilgin Altintas
- *
+ * <p>
  * Effective Java Workshop
- *
- *
  */
 
 /*
@@ -16,20 +14,46 @@ TODO:
  */
 public class NutritionFacts {
 
-    private final int servingSize = 0; // must
-    private final int servings = 0; // must
-    private final int calories = 0; // optional
-    private final int fat = 0;  // optional
-    private final int sodium = 0; // optional
-    private final int carbohydrate = 0; // optional
-        
+    private int servingSize = 0; // must
+    private int servings = 0; // must
+    private int calories = 0; // optional
+    private int fat = 0;  // optional
+    private int sodium = 0; // optional
+    private int carbohydrate = 0; // optional
+
+    public NutritionFacts(int servingSize, int servings) {
+        this(servingSize, servings, 0);
+    }
+
+    public NutritionFacts(int servingSize, int servings, int calories) {
+        this(servingSize, servings, calories, 0);
+    }
+
+    public NutritionFacts(int servingSize, int servings, int calories, int fat) {
+        this(servingSize, servings, calories, fat, 0);
+    }
+
+    public NutritionFacts(int servingSize, int servings, int calories, int fat,
+                          int sodium) {
+        this(servingSize, servings, calories, fat, sodium, 0);
+    }
 
 
-
+    public NutritionFacts(int servingSize, int servings, int calories, int fat,
+                          int sodium, int carbohydrate) {
+        this.servingSize = servingSize;
+        this.servings = servings;
+        this.calories = calories;
+        this.fat = fat;
+        this.sodium = sodium;
+        this.carbohydrate = carbohydrate;
+    }
 
     public static void main(String[] args) {
+        NutritionFacts cocaCola = new NutritionFacts(240, 8, 100, 0, 35, 27);
 
-        
+        System.out.println(cocaCola);
     }
+
 
 }
